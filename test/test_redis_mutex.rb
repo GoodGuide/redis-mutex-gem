6e41d841ef4e3f361bf77ff599e1f27578657327
Ryan Taylor Long <ryan@rtlong.com>
2015-02-02 16:25:59 -0800

    improve tests
    [no story]

--- test/test_redis_mutex.rb
+++ test/test_redis_mutex.rb
@@ -3,6 +3,9 @@ require 'redis_mutex'
 require 'redis'
 
 class TestRedisMutex < Minitest::Test
+  DEFAULT_TIMEOUT = 1
+  LONGER_TIMEOUT = 10
+
   def setup
     # NB: this test suite requires a redis server to be available. set ENV['REDIS_URL'] to change the default hostname/port
     @redis = Redis.new
@@ -22,15 +25,22 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_acquire_lock_succeeds_when_unlocked
-    assert mutex_a.acquire_lock(1)
+    assert mutex_a.acquire_lock(DEFAULT_TIMEOUT)
     assert redis.get(key) == mutex_a.token
-    assert redis.pttl(key) < 1000 # verify expiration was set
+    assert redis.pttl(key) <= DEFAULT_TIMEOUT * 1000 # verify expiration was set
   end
 
   def test_acquire_lock_fails_when_locked
-    mutex_a.acquire_lock(1)
-    assert !mutex_b.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
+    assert !mutex_b.acquire_lock(DEFAULT_TIMEOUT)
     assert redis.get(key) == mutex_a.token
+  end
+
+  def test_token_setter_doesnt_have_side_effects
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
+    original_token = mutex_a.token
+    mutex_a.token = 'foo'
+    assert redis.get(key) == original_token
   end
 
   def test_locked_when_unlocked
@@ -38,17 +48,17 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_locked_when_locked
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
     assert mutex_a.locked?
   end
 
   def test_release_succeeds_when_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
     assert mutex_a.release
   end
 
   def test_release_fails_when_not_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert_raises(RedisMutex::MutexNotLockedError) do
       mutex_b.release
@@ -56,23 +66,23 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_renew_succeeds_when_holding_lock
-    mutex_a.acquire_lock(1)
-    assert mutex_a.renew(10)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
+    assert mutex_a.renew(LONGER_TIMEOUT)
     assert redis.ttl(@key) > 9
   end
 
   def test_renew_fails_when_not_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert_raises(RedisMutex::MutexNotLockedError) do
-      mutex_b.renew(10)
+      mutex_b.renew(LONGER_TIMEOUT)
     end
   end
 
   def test_set_token_succeeds_when_holding_lock
     new_token = 'bar'
 
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert redis.get(key) == mutex_a.token
     assert mutex_a.set_token(new_token)
@@ -81,7 +91,7 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_set_token_fails_when_not_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert_raises(RedisMutex::MutexNotLockedError) do
       mutex_b.set_token('bar')
@@ -89,12 +99,12 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_verify_succeeds_when_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
     assert mutex_a.verify!
   end
 
   def test_verify_fails_when_not_holding_lock
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert_raises(RedisMutex::MutexNotLockedError) do
       mutex_b.verify!
@@ -102,7 +112,7 @@ class TestRedisMutex < Minitest::Test
   end
 
   def test_handoff
-    mutex_a.acquire_lock(1)
+    mutex_a.acquire_lock(DEFAULT_TIMEOUT)
 
     assert RedisMutex.new(redis, mutex_name, mutex_a.token).release
   end
